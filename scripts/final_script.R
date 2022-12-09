1dc80246672e2a7c922047f990e8817e2acf79fe
Haoming Deng <haomingdeng@umail.ucsb.edu>
2022-12-08 22:29:46 -0800

    update

--- scripts/final_script.R
+++ scripts/final_script.R
@@ -1,21 +1,15 @@
-# Ankur 
 install.packages("RMariaDB")
 install.packages("DBI")
 library(RMariaDB)
 library(DBI)
-
 df_day <- read.csv("data/day.csv", header=TRUE)
 head(df_day)
-
 df_hour <-read.csv("data/hour.csv", header=TRUE)
 head(df_hour)
 
-install.packages("RMySQL")
-## Create a database
-# connect R to MySQL by creating a MySQL connection object
 con <- dbConnect(RMariaDB::MariaDB(), 
                  user = 'root', 
-                 password = 'password',
+                 password = 'password', # YOUR PASSWORD HERE
                  host = 'localhost')
 
 # create a new database
@@ -42,14 +36,32 @@ dbWriteTable(conn = mydb,
 dbListTables(mydb)
 
 # list the fields in a table
-dbListFields(con, "day")
-dbListFields(con, 'hour')
+dbListFields(mydb, "day")
 
-# view the whole table
-dbReadTable(con, "day")
-dbReadTable(mydb, 'hour')
-dbReadTable(mydb, 'day')
-------------------------------------------------
+dbReadTable(mydb, "day")
+
+# Basic Syntax
+# SELECT
+# we want to select day.season and day.yr from day. we can also select other columns from the table.
+dbGetQuery(mydb, 'SELECT day.season, day.yr FROM day')
+
+# INSERT
+# we want to insert new values 1 and 0 into day.season and day.yr
+dbGetQuery(mydb, 'INSERT INTO day(day.season, day.yr)
+                  VALUES (1,0)')
+
+
+# UPDATE 
+# we want to update weekday to 9 where workingday equals to 0 in day. we can also use multiple conditions in the WHERE clause.
+dbGetQuery(mydb, 'UPDATE day
+                  SET weekday=8
+                  WHERE workingday=0')
+
+# DELETE
+# we want to delete values from day where workingday equals to 0. we can also set other conditions when we want to delete values from the table.
+dbGetQuery(mydb, 'DELETE FROM day
+                  WHERE workingday=0')
+
 # Clause 
 # WHERE CLAUSE 
 # Over here we are trying to find the instant where the temp is at an exact value. So our output will be the instant
@@ -129,4 +141,102 @@ dbGetQuery(mydb, "SELECT windspeed, CASE
 # DISTINCT 
 # The SELECT DISTINCT statement is used to return only distinct (different) values.
 dbGetQuery(mydb, "SELECT DISTINCT(dteday) FROM day where temp = 0.3441670" ) 
-dbGetQuery(mydb, "SELECT DISTINCT(windspeed) FROM day where temp = 0.1" ) # DNE
+dbGetQuery(mydb, "SELECT DISTINCT(windspeed) FROM day where temp = 0.1" ) 
+
+#rand()
+dbGetQuery(mydb, "SELECT RAND()" )
+
+#round()
+dbGetQuery(mydb, "SELECT temp,round(temp,3),windspeed, round(windspeed,
+3) FROM day" )
+
+#FLOOR()
+dbGetQuery(mydb,"SELECT AVG(windspeed),MIN(windspeed),FLOOR(AVG(windspeed)) AS
+Lower_Average FROM day" ) 
+dbGetQuery(mydb, "SELECT
+AVG(windspeed),MIN(windspeed),FLOOR(MIN(windspeed)) AS Lower_Average
+FROM day" )
+
+# CEIL()
+dbGetQuery(mydb, "SELECT
+AVG(windspeed),MIN(windspeed),CEIL(AVG(windspeed)) AS Lower_Average FROM
+day" ) 
+dbGetQuery(mydb, "SELECT
+AVG(windspeed),MIN(windspeed),CEIL(MIN(windspeed)) AS Lower_Average FROM
+day" )
+
+# Truncate(n,d) with positive d
+dbGetQuery(mydb, "SELECT atemp, hum,
+TRUNCATE(atemp,2), TRUNCATE(hum,1) FROM day" )
+
+# Truncate(n,d) with negative d we want to see atem and hum are truncated
+dbGetQuery(mydb,
+           "SELECT atemp, hum, TRUNCATE(atemp,-2), TRUNCATE(hum,-1) FROM day" )
+
+# Count ALL: How many row we have
+dbGetQuery(mydb, "SELECT COUNT(*) FROM day" )
+
+# Count Distinct weekday 
+dbGetQuery(mydb, "SELECT COUNT(DISTINCT (weekday)) FROM day" )
+
+# we want to see the average windspeed among all data
+
+dbGetQuery(mydb, "SELECT AVG(windspeed) AS AVERAGE_SPEED FROM day" )
+
+# we want to see the sum of casual users
+dbGetQuery(mydb, "SELECT SUM(casual) AS TOTAL_NUM_CASUAL_USER FROM day")
+
+# we want to see the max speed of wind
+dbGetQuery(mydb, "SELECT MAX(windspeed) AS MAX_WINDSPEED FROM day")
+
+# we want to see the min speed of wind
+dbGetQuery(mydb, "SELECT MIN(windspeed) AS MIN_WINDSPEED FROM day")
+
+# We want to concat days and hours as one column
+
+dbGetQuery(mydb, "SELECT CONCAT(dteday,' Hour: ',hr) FROM hour")
+
+# We want to see how many characters composes the date
+dbGetQuery(mydb, "SELECT DISTINCT(LENGTH(dteday)) AS length_of_char_date FROM hour")
+
+#WE want to firstly create windspeed breakdown to two part low and high. Then we want to 
+# show how to use lower function to make all spring be lower case
+dbGetQuery(mydb, "SELECT LOWER(SPEED_BREAKDOWN) FROM (SELECT windspeed, CASE 
+            WHEN windspeed < 0.1 THEN 'LOW'
+            ELSE 'HIGH'
+            END AS SPEED_BREAKDOWN FROM day) AS tmp")
+
+#  We want to show how to use upper function to make all spring be upper case
+dbGetQuery(mydb, "SELECT UPPER(SPEED_BREAKDOWN) FROM (SELECT windspeed, CASE 
+            WHEN windspeed < 0.1 THEN 'low'
+            ELSE 'high'
+            END AS SPEED_BREAKDOWN FROM day) AS tmp")
+
+library(dbplyr)
+library(dplyr)
+#CONNECT TO DATABASE
+con <- dbConnect(RMariaDB::MariaDB(), 
+                 user = 'root', 
+                 password = 'shiatsing2',
+                 host = 'localhost',
+                 dbname = 'bikes')
+
+#CONNECT TO TABLE
+data_sample<- con %>% tbl("day")
+
+
+# SELECT differenct columns 
+# SELECT 4 cloumns together. In Sql we have to write down the four columns' name seperately. But in dbplyr, we can use : to select a range of columns.
+data_sample %>% select(season:holiday)
+
+# We can use filter to subsititude where condition in sql. Here we are showing data that has wind speed greater than 0.4
+data_sample %>% filter(windspeed > 0.4)
+
+# We can also do the same SQL group by and aggregation task by uing dbplyr group_by function
+
+data_sample %>% group_by(weekday) %>%
+  summarise(
+    count_day = count(instant)
+  )
+
+
